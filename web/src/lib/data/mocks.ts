ec3e8e01469dd552bbbb21ca0ffe4bba0eae1943
francisco-solis99 <franciscosoliscss@gmail.com>
2025-05-16 21:08:21 -0600

    [chore] add the mock data for tests

---
+++ web/src/lib/data/mocks.ts
@@ -0,0 +1,79 @@
+export const contextOpts = [
+  {
+    label: "Solo",
+    name: "solo",
+  },
+  {
+    label: "Pareja",
+    name: "pareja",
+  },
+  {
+    label: "Familia",
+    name: "familia",
+  },
+  {
+    label: "Amigos",
+    name: "amigos",
+  },
+];
+
+export const moodOpts = [
+  {
+    label: "Modo Fan",
+    name: "modo fan",
+  },
+  {
+    label: "Explorador",
+    name: "explorador",
+  },
+  {
+    label: "Emociones Fuertes",
+    name: "buscando emociones fuertes",
+  },
+  {
+    label: "Relajado",
+    name: "relajado",
+  },
+  {
+    label: "Cita",
+    name: "modo cita",
+  },
+  {
+    label: "Reír",
+    name: "quiero reirme",
+  },
+  {
+    label: "Curioso",
+    name: "curioso por algo distinto",
+  },
+  {
+    label: "Nostálgico",
+    name: "modo nostálgico",
+  },
+  {
+    label: "Profundo",
+    name: "buscando profundidad",
+  },
+];
+
+export const genres = [
+  { name: "Acción", label: "Acción" },
+  { name: "Aventura", label: "Aventura" },
+  { name: "Animación", label: "Animación" },
+  { name: "Comedia", label: "Comedia" },
+  { name: "Crimen", label: "Crimen" },
+  { name: "Documental", label: "Documental" },
+  { name: "Drama", label: "Drama" },
+  { name: "Familia", label: "Familia" },
+  { name: "Fantasía", label: "Fantasía" },
+  { name: "Historia", label: "Historia" },
+  { name: "Terror", label: "Terror" },
+  { name: "Música", label: "Música" },
+  { name: "Misterio", label: "Misterio" },
+  { name: "Romance", label: "Romance" },
+  { name: "Ciencia ficción", label: "Ciencia ficción" },
+  { name: "Película de TV", label: "Película de TV" },
+  { name: "Suspense", label: "Suspense" },
+  { name: "Bélica", label: "Bélica" },
+  { name: "Western", label: "Western" },
+];
