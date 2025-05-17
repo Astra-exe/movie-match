e14028e3f36eefab4a2cc6b458de614c1e2361ed
francisco-solis99 <franciscosoliscss@gmail.com>
2025-05-16 21:11:24 -0600

    [chore] add the explore page with initial content

---
+++ web/src/app/explore/page.tsx
@@ -0,0 +1,11 @@
+import Footer from "@/components/home-page/Footer";
+
+export default function ExplorePage() {
+  return (
+    <div>
+      <h1>Generate your next selection movie</h1>
+
+      <Footer></Footer>
+    </div>
+  );
+}
