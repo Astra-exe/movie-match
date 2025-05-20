468f5dc1bb99e6e3e33d4128a91df9b9cb81742c
francisco-solis99 <franciscosoliscss@gmail.com>
2025-05-19 20:34:35 -0600

    [chore] add some types to use in the selections CRUD

---
+++ web/src/lib/data/types.ts
@@ -0,0 +1,33 @@
+interface SelectionMovieInfo {
+  id: string;
+  title: string;
+  overview: string;
+  genres: string[];
+  posterPath: string;
+  backdropPath: string;
+  youtubeKey: string;
+  // releaseDate: string
+}
+
+interface SelectionInfo {
+  id: string;
+  visitName: string;
+  isPublic: boolean;
+  creationDate: string;
+  context: string;
+  mood: string;
+  why: string;
+  affinity: number;
+  people: {
+    name: string;
+    genre: string;
+    emojis: string[];
+  }[];
+}
+
+export interface SelectionPick {
+  selectionInfo: SelectionInfo;
+  movieInfo: SelectionMovieInfo;
+}
+
+type SelectionPickPublic = Omit<SelectionPick, "why" | "isPublic" | "">;
