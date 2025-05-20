94f4cab837fc2eb07bed0376a1f35279fc27f689
francisco-solis99 <franciscosoliscss@gmail.com>
2025-05-20 14:07:53 -0600

    [fix] conditional render if there is not movie ioptiosn generated

--- web/src/app/results/page.tsx
+++ web/src/app/results/page.tsx
@@ -46,43 +46,42 @@ export default async function ResultsPage() {
     const decoded = Buffer.from(cookie.value, "base64").toString("utf-8");
     movieRecommendations = JSON.parse(decoded);
   }
-  if (!movieRecommendations) {
-    return <p>No se encontraron películas</p>;
+
+  let movieOptions: any[] = [];
+  if (movieRecommendations) {
+    const movieOptionsPromise = movieRecommendations.recommendations.map(
+      async (item: GenerateResult) => {
+        const { affinity, genres, movie_id, why } = item;
+        const movieInfo = await getMovieInfoById(movie_id);
+        const {
+          title: titleMovie,
+          overview,
+          release_date,
+          poster_path,
+          backdrop_path,
+          youtube_video_key,
+        } = movieInfo;
+
+        // Mix the resultsData and the movie info to render the movie cards options
+        return {
+          id: String(movie_id),
+          title: String(titleMovie ?? ""),
+          overview: String(overview ?? ""),
+          genres,
+          releaseDate: String(release_date ?? ""),
+          posterPath: poster_path ?? "",
+          backdropPath: backdrop_path ?? "",
+          youtubeVideoKey: String(youtube_video_key ?? ""),
+          affinity: Number(((affinity ?? 0) * 100).toFixed(2)),
+          why: String(why ?? ""),
+        };
+      }
+    );
+    // Use Promise.all to wait for all movie info promises to resolve with await
+    movieOptions = await Promise.all(movieOptionsPromise);
+    console.log({ movieOptions, info: movieRecommendations.info });
   }
 
-  const movieOptionsPromise = movieRecommendations.recommendations.map(
-    async (item: GenerateResult) => {
-      const { affinity, genres, movie_id, why } = item;
-      const movieInfo = await getMovieInfoById(movie_id);
-      const {
-        title: titleMovie,
-        overview,
-        release_date,
-        poster_path,
-        backdrop_path,
-        youtube_video_key,
-      } = movieInfo;
-
-      // Mix the resultsData and the movie info to render the movie cards options
-      return {
-        id: String(movie_id),
-        title: String(titleMovie ?? ""),
-        overview: String(overview ?? ""),
-        genres,
-        releaseDate: String(release_date ?? ""),
-        posterPath: poster_path ?? "",
-        backdropPath: backdrop_path ?? "",
-        youtubeVideoKey: String(youtube_video_key ?? ""),
-        affinity: Number(((affinity ?? 0) * 100).toFixed(2)),
-        why: String(why ?? ""),
-      };
-    }
-  );
-
-  // Use Promise.all to wait for all movie info promises to resolve with await
-  // and handle errors
-  const movieOptions = await Promise.all(movieOptionsPromise);
-  console.log({ movieOptions, info: movieRecommendations.info });
   // Handle errors if any of the promises fail
   if (movieOptions.some((movie) => movie === null)) {
     throw new Error("Error fetching movie data");
@@ -93,34 +92,37 @@ export default async function ResultsPage() {
       <Header />
       <div className="bg-background text-foreground pb-32">
         <header className="w-[90%] mx-auto">
-          <div className="py-8 md:py-14 lg:py-20 text-center max-w-2xl mx-auto">
-            <h1 className="text-4xl font-bold text-primary">
+          <div className="py-8 md:py-14 lg:py-20 text-center max-w-2xl mx-auto space-y-2">
+            <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-primary">
               Tus recomendaciones de películas
             </h1>
             <p className="mb-3 text-lg text-gray-300">
-              Revisa las opciones que encontramos para ti según tu información y elige tu favorita. Estamos seguros de que la disfrutarás.
+              Revisa las opciones que encontramos para ti según tu información y
+              elige tu favorita. Estamos seguros de que la disfrutarás.
             </p>
           </div>
         </header>
         <main>
           <div>
-            {movieOptions.length === 0 && (
+            {!movieRecommendations || movieOptions.length === 0 ? (
               <div className="text-center py-8 text-xl">
-                No se encontraron películas que coincidan con tus preferencias. Por favor intenta de nuevo.
+                No se encontraron películas que coincidan con tus preferencias.
+                Por favor intenta de nuevo.
               </div>
+            ) : (
+              <Suspense
+                fallback={
+                  <div className="text-center py-10 text-lg">
+                    Cargando recomendaciones...
+                  </div>
+                }
+              >
+                <MovieOptions
+                  movieList={movieOptions}
+                  infoVisit={movieRecommendations.info}
+                />
+              </Suspense>
             )}
-            <Suspense
-              fallback={
-                <div className="text-center py-10 text-lg">
-                  Cargando recomendaciones...
-                </div>
-              }
-            >
-              <MovieOptions
-                movieList={movieOptions}
-                infoVisit={movieRecommendations.info}
-              />
-            </Suspense>
           </div>
         </main>
       </div>
