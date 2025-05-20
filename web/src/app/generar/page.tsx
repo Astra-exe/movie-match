5fa7b876c47aae98930e97cb38d92eb3d12f4271
francisco-solis99 <franciscosoliscss@gmail.com>
2025-05-20 13:36:20 -0600

    [chore] chnage the route name in the generar page

---
+++ web/src/app/generar/page.tsx
@@ -0,0 +1,27 @@
+import Header from "@/components/home-page/Header";
+import Footer from "@/components/home-page/Footer";
+import SelectForm from "./_components/SelectForm";
+
+export default function GeneratePage() {
+  return (
+    <div className="w-full min-h-screen min-w-[320px] font-[family-name:var(--font-geist-sans)]">
+      <Header />
+      <div className="bg-background text-foreground pb-32">
+        <header className="w-[90%] mx-auto">
+          <div className="py-8 md:py-14 lg:py-20 text-center max-w-2xl mx-auto">
+            <h1 className="text-4xl font-bold text-primary">
+              Genera tu selección de películas
+            </h1>
+            <p className="mb-3 text-lg text-gray-300">
+              Utiliza nuestra herramienta potenciada con IA para descubrir la película perfecta para ver, personalizada según los detalles de tu visita.
+            </p>
+          </div>
+        </header>
+        <main>
+          <SelectForm />
+        </main>
+      </div>
+      <Footer />
+    </div>
+  );
+}
