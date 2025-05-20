826eb9853df88c22d1e9a3120254aa1233b412d4
francisco-solis99 <franciscosoliscss@gmail.com>
2025-05-19 21:06:13 -0600

    [fix] change the response removing the mock data

--- web/src/actions/generate.ts
+++ web/src/actions/generate.ts
@@ -5,7 +5,7 @@ import { cookies } from "next/headers";
 import type { SelectionData } from "@/lib/data/schema";
 
 // TODO: delete this mock data
-import { generateOpts } from "@/lib/data/mocks";
+// import { generateOpts } from "@/lib/data/mocks";
 
 interface RecommendationGenerated {
   affinity: number;
@@ -54,20 +54,20 @@ export async function submitFormSelectMovie(
       comments,
     };
 
-    return {
-      success: true,
-      data: {
-        ...generateOpts,
-        info: {
-          visit: name,
-          context: context ?? "",
-          mood: mood ?? "",
-          comments: comments ?? "",
-          people: people ?? [],
-        },
-      },
-      message: "Your movie options has been successfully selected!",
-    };
+    // return {
+    //   success: true,
+    //   data: {
+    //     ...generateOpts,
+    //     info: {
+    //       visit: name,
+    //       context: context ?? "",
+    //       mood: mood ?? "",
+    //       comments: comments ?? "",
+    //       people: people ?? [],
+    //     },
+    //   },
+    //   message: "Your movie options has been successfully selected!",
+    // };
 
     const response = await fetch(URL_API_ANALIZE, {
       method: "POST",
@@ -81,11 +81,20 @@ export async function submitFormSelectMovie(
       throw new Error("Failed to submit data");
     }
 
-    const data = await response.json();
+    const dataResults = await response.json();
 
     return {
       success: true,
-      data,
+      data: {
+        ...dataResults,
+        info: {
+          visit: name,
+          context: context ?? "",
+          mood: mood ?? "",
+          comments: comments ?? "",
+          people: people ?? [],
+        },
+      },
       message: "Your movie options has been successfully selected!",
     };
   } catch (error) {
